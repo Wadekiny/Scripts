0d04c3f47d1ca0e414ea2d2e2eebb7584d585f67
wadekiny <wadekiny@qq.com>
2023-02-03 13:01:35 +0800

    wallpic

--- wallpic.py
+++ wallpic.py
@@ -6,11 +6,16 @@ bg_dir = '/home/wadekiny/Pictures/background/'
 time_cycle = 1000
 bg_list = glob.glob(bg_dir + '*.jpg') + glob.glob(bg_dir + '*.png')
 
+os.system('swww init')
+time.sleep(5)
 while True:
-    bg_list = glob.glob(bg_dir + '*.jpg') + glob.glob(bg_dir + '*.png')
+    # bg_list = glob.glob(bg_dir + '*.jpg') + glob.glob(bg_dir + '*.png')
     for pic_name in bg_list:
         print(pic_name)
-        os.system('feh --bg-fill '+ pic_name)
+        # os.system('feh --bg-fill '+ pic_name)
+        cmd = 'swww img {} --transition-type center --transition-fps 255 --transition-step 25'.format(pic_name)
+        print(cmd)
+        os.system(cmd)
         time.sleep(time_cycle)
 
 
