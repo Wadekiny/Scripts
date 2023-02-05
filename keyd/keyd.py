4fef5abef6aa7bd89f76939150c3cbecbfddab79
wadekiny <wadekiny@qq.com>
2023-02-05 16:56:31 +0800

    delete comment

--- keyd/keyd.py
+++ keyd/keyd.py
@@ -28,26 +28,6 @@ from selectors import DefaultSelector, EVENT_READ
 # || /dev/input/event1 Lid Switch PNP0C0D/button/input0
 # || /dev/input/event0 Power Button PNP0C0C/button/input0
 
-# 找到所需设备
-# devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
-# for device in devices:
-#     if "Keyboard" in device.name or "keyboard" in device.name:
-#         print(device.path,"--", device.name,"--", device.phys)
-#     if "keyd virtual" in device.name:
-#         print(device.path,"--", device.name,"--", device.phys)
-
-# device = evdev.InputDevice("/dev/input/event21") # virtual mouse
-# device = evdev.InputDevice("/dev/input/event20") # virtual keyboard
-# keyboard1 = evdev.InputDevice("/dev/input/event2") #自带键盘
-# keyboard2 = evdev.InputDevice("/dev/input/event4") # 外接键盘
-# os.system('notify-send hh')
-# #读取键盘事件
-# for event in device.read_loop():
-#     if event.type == evdev.ecodes.EV_KEY:
-#         print("isthis!",evdev.categorize(event))
-
-
-
 class TargetKey():
     def __init__(self,_code,_type,_value):
         self._code = _code 
@@ -79,6 +59,16 @@ class KeyboardRemap(object):
                 # print(device.path,device.name,device.phys)
         self.remap()
 
+    def print_device_event(self, device_path:str="/dev/input/event20"):
+        device = evdev.InputDevice(device_path)
+        for event in device.read_loop():
+            if event.type == evdev.ecodes.EV_KEY:
+                print("isthis!:",evdev.categorize(event))
+
+    def print_all_device(self):
+        for device in self._devices: 
+            print(device.path, device.name, device.phys)
+
     def save_flag(self):
         flag = self._swap_flag
         path = self._save_flag_path
@@ -101,14 +91,14 @@ class KeyboardRemap(object):
         if self._waybar_signal_flag:
             subprocess.Popen(self._waybar_update_command,shell=True)
 
-    def loop_detector(self):
+    def loop_detector(self, is_print_all_event:bool=False):
         try:
             while True:
                 last_flag = self._swap_flag
                 for key, mask in self._selector.select(): #设备循环
                     device = key.fileobj
                     for event in device.read(): #事件循环
-                        # print(event) #用来查看按键code
+                        if is_print_all_event: print(event) #用来查看按键code
                         for toggle_key in self._toggle_keys: #触发键循环
                             if toggle_key.match_key(event):
                                 self._swap_flag = not self._swap_flag
@@ -148,103 +138,10 @@ remapper = KeyboardRemap([toggle_key],
 
 print('running...')
 remapper.loop_detector()
-
-print('terminated')
-#try:
-#    while True:
-#        # 扫描键盘事件, 更新asd2num_flag
-#        last_flag = asd2num_flag
-#        for key, mask in selector.select():
-#            device = key.fileobj
-#            for event in device.read():
-#                # print(event) #用来查看按键code
-#                if toggle_target.match_key(event):
-#                    asd2num_flag = not asd2num_flag
-#                if quit_target.match_key(event):
-#                    asd2num_flag = False
-#        if last_flag != asd2num_flag:
-#            # flag更新了 #需要root权限 
-#            remap(asd2num_flag)
-#except:
-#    remap(False)
-#    os.system("sudo systemctl stop keyd")
+print('TERMINATED')
 
 
 
-##保存成文件，或者保存成全局的环境变量
-#def save_flag(flag):
-#    path = '/home/wadekiny/Scripts/keyd/asd2num_flag.txt'
-#    with open(path,'w') as f:
-#        if flag : f.write("export _asd2num='ASD2NUM'")
-#        else :f.write("export _asd2num='NORMAL'")
-#
-#def remap(flag,waybar_signal = True):
-#    print("flag:", flag, "| waybar_signal", waybar_signal)
-#    waybar_update_command = "pkill -RTMIN+8 waybar"
-#    if flag:
-#        asd = ['a','s','d','f','g','h','j','k','l',';']
-#        num = ['1','2','3','4','5','6','7','8','9','0']
-#        # subprocess.popen()
-#        for i in range(len(num)):
-#            subprocess.Popen("keyd -e '{}={}'".format(asd[i],num[i]), shell=True)
-#            subprocess.Popen("keyd -e '{}={}'".format(num[i],asd[i]), shell=True)
-#            save_flag(asd2num_flag)
-#    else:
-#        subprocess.Popen("keyd -e reset", shell=True)
-#        save_flag(asd2num_flag)
-#    if waybar_signal:
-#        subprocess.Popen(waybar_update_command,shell=True)
-#
-#
-#print('starting keyd...')
-#os.system("sudo systemctl start keyd")
-#print('waiting keyd...')
-#time.sleep(5) #等待keyd启动
-#print('scanning device...')
-#selector = DefaultSelector()
-#devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
-#for device in devices:
-#    if "Keyboard" in device.name or "keyboard" in device.name:
-#        selector.register(device,EVENT_READ)
-#        print(device.path, device.name, device.phys)
-#    if "keyd virtual" in device.name and "pointer" not in device.name:
-#        selector.register(device,EVENT_READ)
-#        print(device.path,device.name,device.phys)
-#
-#
-#
-## 启用keyd之后，按键code也会变(并没有，是默认将rightshift映射到了leftshift)
-## quit_target = target(58, 1, 0);    #close  esc
-## toggle_target = target(100, 1, 0);  #toggle right fn
-#quit_target = TargetKey(58, 1, 0);    #close  esc
-#toggle_target = TargetKey(54, 1, 0);  #toggle shift_r
-#asd2num_flag = False 
-#
-#print("init all settings")
-#remap(asd2num_flag)
-#
-#try:
-#    while True:
-#        # 扫描键盘事件, 更新asd2num_flag
-#        last_flag = asd2num_flag
-#        for key, mask in selector.select():
-#            device = key.fileobj
-#            for event in device.read():
-#                # print(event) #用来查看按键code
-#                if toggle_target.match_key(event):
-#                    asd2num_flag = not asd2num_flag
-#                if quit_target.match_key(event):
-#                    asd2num_flag = False
-#        if last_flag != asd2num_flag:
-#            # flag更新了 #需要root权限 
-#            remap(asd2num_flag)
-#except:
-#    remap(False)
-#    os.system("sudo systemctl stop keyd")
-#
-#
-#
-#
 ## 问题：需不需要等待子进程完成
 ## os.system("nohup sudo systemctl start keyd &" 可以实现异步执行, 或者subprocess
 ## 更改映射，读不到按键了,因为设备变成了keyd virtual device?
